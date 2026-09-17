92c67db637851887d27daaa351d4ce398223158b
dreinst <asteinec@gmail.com>
2026-09-17 21:28:10 +0700

    Ganti logo D'Pro di footer dengan versi SVG hitam
    Menggunakan logo vektor resmi terbaru untuk background terang.

--- src/komponen/ui/KakiHalaman.tsx
+++ src/komponen/ui/KakiHalaman.tsx
@@ -2,8 +2,8 @@
 export default function KakiHalaman({ className = "" }: { className?: string }) {
   return (
     <footer className={`flex flex-col items-center gap-2 text-center text-[11px] text-slate-400 ${className}`}>
-      {/* eslint-disable-next-line @next/next/no-img-element -- PNG statis kecil, tanpa optimasi */}
-      <img src="/logo/dpro-logo.png" alt="D'Production Event Organizer" className="h-9 w-auto" />
+      {/* eslint-disable-next-line @next/next/no-img-element -- vector logo, tanpa optimasi */}
+      <img src="/logo/dpro-logo.svg" alt="D'Production Event Organizer" className="h-9 w-auto" />
       <p>Made by dreinst, organized by D&rsquo;Production Event Organizer</p>
     </footer>
   );
